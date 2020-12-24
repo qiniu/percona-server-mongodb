94e3f032964f862cded5e4ab94ad44f155779e82
shenmengye <shenmengye@qiniu.com>
2020-12-24 15:52:11 +0800

    build(src/mongo/client/connpool.cpp): 编译错误

--- src/mongo/client/connpool.cpp
+++ src/mongo/client/connpool.cpp
@@ -307,7 +307,7 @@ DBClientBase* DBConnectionPool::get(const string& host, double socketTimeout) {
         if (p.openConnections() >= this->_maxInUse) {
             log() << "Too many in-use connections; waiting until there are fewer than "
                   << this->_maxInUse;
-            uassert(17289, "Too many in-use connections; waiting until there are fewer than " + std::to_string(this->_maxInUse), false);
+            uassert(17299, "Too many in-use connections; waiting until there are fewer than " + std::to_string(this->_maxInUse), false);
         }
     }
 
@@ -339,7 +339,7 @@ DBClientBase* DBConnectionPool::get(const MongoURI& uri, double socketTimeout) {
         if (p.openConnections() >= this->_maxInUse) {
             log() << "Too many in-use connections; waiting until there are fewer than "
                   << this->_maxInUse;
-            uassert(17289, "Too many in-use connections; waiting until there are fewer than " + std::to_string(this->_maxInUse), false);
+            uassert(17389, "Too many in-use connections; waiting until there are fewer than " + std::to_string(this->_maxInUse), false);
         }
     }
 
