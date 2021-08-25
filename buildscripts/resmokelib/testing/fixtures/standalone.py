2cd670e7907191c827003abd0d6ff72158b7b666
shenmengye <shenmengye@qiniu.com>
2021-08-25 18:46:55 +0800

    port冲突

--- buildscripts/resmokelib/testing/fixtures/standalone.py
+++ buildscripts/resmokelib/testing/fixtures/standalone.py
@@ -96,6 +96,7 @@ class MongoDFixture(interface.Fixture):
     def check_port(self, port):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             s.bind(("127.0.0.1", port))
             s.close()
             return True
