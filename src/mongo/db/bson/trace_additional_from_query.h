bab3fbd24846df2a84a0b73062727827861b7ad1
lixin <lixin@qiniu.com>
2022-03-28 16:17:27 +0800

    去掉没用的注释

--- src/mongo/db/bson/trace_additional_from_query.h
+++ src/mongo/db/bson/trace_additional_from_query.h
@@ -1,9 +1,9 @@
 /*
  * @Author: your name
  * @Date: 2022-03-11 16:17:45
- * @LastEditTime: 2022-03-11 16:27:08
+ * @LastEditTime: 2022-03-28 16:15:49
  * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 从query的bson中提取为kodo定制的附加信息bson
  * @FilePath: /percona-server-mongodb/src/mongo/db/bson/trace_additional_from_query.h
  */
 #pragma once
