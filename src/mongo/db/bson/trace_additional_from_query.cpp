bc37a3bb2ee4d3bbf980da3658d4d01364777b51
lixin <lixin@qiniu.com>
2022-05-24 16:20:36 +0800

    细节修改

--- src/mongo/db/bson/trace_additional_from_query.cpp
+++ src/mongo/db/bson/trace_additional_from_query.cpp
@@ -3,11 +3,12 @@
 #include "mongo/db/bson/trace_additional_from_query.h"
 namespace mongo {
 namespace trace_query {
+const std::string KODOMSG = "@kodoMsg@";
 bool traceAdditionalInfoFromQuery(BSONObj& query, BSONObj& additional){
     bool ret = false;
-    if (query.hasElement("@kodoMsg@")) {
-        additional = query.getObjectField("@kodoMsg@");
-        query = query.removeField("@kodoMsg@");
+    if (query.hasElement(KODOMSG)) {
+        additional = query.getObjectField(KODOMSG);
+        query = query.removeField(KODOMSG);
         ret = true;
     }
     return ret;
